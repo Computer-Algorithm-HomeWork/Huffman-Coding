f2c38613b9fcf6472be378d60a0789dd6ece8940
na0918 <nby0918@naver.com>
2021-04-10 21:07:15 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,45 @@
-public class Main {
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
+class Node {
+    public int freq;
+    public Node rNode;
+    public Node lNode;
+}
+
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        HashMap<Character, Integer> fre = new HashMap<>();
+
+        String str;
+        str = sc.nextLine();
+
+        for(int i=0; i<str.length(); i++) {
+            char st = str.charAt(i);
+
+            if(fre.containsKey(st))
+                fre.put(st, fre.get(st)+1);
+            else fre.put(st, 1);
+        }
+
+        Collection<Integer> values = fre.values();
+        for (Integer value : values) {
+            pq.add(value);
+        }
+
+            while (pq.size() >= 2) {
+                Node n = new Node();
+
+                n.lNode = pq.poll();
+                n.rNode = pq.poll();
+                n.freq = n.lNode.freq + n.rNode.freq;
+
+                pq.add(n.freq);
+            }
+            System.out.println(pq.peek());
     }
 }
